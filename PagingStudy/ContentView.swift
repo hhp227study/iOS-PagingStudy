8942a2e9fd1a3431a0cba5d5851650102df5db3d
hhp227 <hong227@naver.com>
2022-06-11 21:03:18 +0900

    Initial commit

---
+++ PagingStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PagingStudy
+//
+//  Created by 홍희표 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PagingStudy/PagingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagingStudyApp.swift
+//  PagingStudy
+//
+//  Created by 홍희표 on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct PagingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
